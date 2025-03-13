74d3f68e1240cc681a6f45c5b97507da0151ee76
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-03-13 17:35:30 +0530

    feat: add initial components and configuration for React and Vite setup

---
+++ redux-with-react/src/App.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Amount from './components/Amount';
+import Bonus from './components/Bonus';
+
+function App() {
+
+  return (
+    <div>
+      <Amount />
+      <Bonus />
+    </div>
+  )
+}
+
+export default App;
