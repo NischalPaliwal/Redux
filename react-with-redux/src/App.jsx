785cced3d733986479093035174421bf86a80a45
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-03-13 18:33:58 +0530

    code added

--- react-with-redux/src/App.jsx
+++ react-with-redux/src/App.jsx
@@ -1,8 +1,14 @@
+import Amount from './components/Amount';
+import Bonus from "./components/Bonus";
+
 function App() {
 
   return (
     <div className="min-h-screen flex justify-center items-center">
-      
+      <div className=''>
+      <Amount />
+      <Bonus />
+      </div>
     </div>
   )
 }
