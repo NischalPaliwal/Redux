c91135b6bbbada846a95c65fed1432c2e1cb7fe1
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-03-16 17:18:37 +0530

    feat: add selectors for cart items and totals in Redux store

---
+++ redux-project/src/lib/features/cart/cartSelectors.ts
@@ -0,0 +1,9 @@
+import { RootState } from "../../store";
+
+export const selectCartItems = (state : RootState) => state.cart.items;
+
+export const selectCartTotal = (state : RootState) => 
+    state.cart.items.reduce((total, item) => total + item.price * item.quantity, 0);
+
+export const selectCartItemTotal = (state : RootState) => 
+    state.cart.items.reduce((count, item) => count + item.quantity, 0);
